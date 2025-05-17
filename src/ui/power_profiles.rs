bd88b2e626d7b4ffb95ca2ec62b854171c896065
A. Taha Baki <atahabaki.dev@pm.me>
2025-05-17 05:22:21 +0300

    add: ui has logic how sweet

--- src/ui/power_profiles.rs
+++ src/ui/power_profiles.rs
@@ -1,12 +1,16 @@
 use crate::GlimpsOSD;
 
 impl GlimpsOSD {
-    pub(crate) fn osd_power_profile() -> gtk::Label {
+    pub(crate) fn osd_power_profile(state: String) -> gtk::Label {
+        let label = match &state[..] {
+            "power-saver" => "  Power-Saver",
+            "balanced" => "  Balanced",
+            "performance" => " Performance",
+            _ => "",
+        };
         gtk::Label::builder()
-            .css_classes(vec!["power", "power-saver"])
-            .label("  Power-Saver")
-            // .label("  Balanced")
-            // .label(" Performance")
+            .css_classes(vec!["power", state.clone().as_str()])
+            .label(label)
             .build()
     }
 }
