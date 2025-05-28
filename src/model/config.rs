f15b6be94d946c26bab263c98519bdcc6e5c626b
A. Taha Baki <atahabaki.dev@pm.me>
2025-05-29 00:41:19 +0300

    add: impl Default for Configuration

--- src/model/config.rs
+++ src/model/config.rs
@@ -9,15 +9,16 @@ pub(crate) struct Configuration {
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct Positioning {
     anchor: Anchor,
-    top_margin: Margin,
-    bottom_margin: Margin,
-    left_margin: Margin,
-    right_margin: Margin,
+    top_margin: Option<Margin>,
+    bottom_margin: Option<Margin>,
+    left_margin: Option<Margin>,
+    right_margin: Option<Margin>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub(crate) enum Anchor {
     Top,
+    #[default]
     Bottom,
     Left,
     Right,
@@ -32,6 +33,21 @@ impl From<Anchor> for gtkls::Edge {
             Anchor::Bottom => gtkls::Edge::Bottom,
             Anchor::Left => gtkls::Edge::Left,
             Anchor::Right => gtkls::Edge::Right,
+        }
+    }
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            duration: 500,
+            positioning: Positioning {
+                anchor: Anchor::default(),
+                top_margin: None,
+                bottom_margin: Some((Anchor::default(), 50)),
+                left_margin: None,
+                right_margin: None,
+            },
         }
     }
 }
