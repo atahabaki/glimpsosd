96005b8f71905dbba79206040bbb314bf47d0b40
A. Taha Baki <atahabaki.dev@pm.me>
2025-06-01 10:53:27 +0300

    add&rm: seperate logic & ui

--- src/ui/power_profiles.rs
+++ src/ui/power_profiles.rs
@@ -1,12 +1,6 @@
-pub(crate) fn osd_power_profile(state: String) -> gtk::Label {
-    let label = match &state[..] {
-        "power-saver" => "  Power-Saver",
-        "balanced" => "  Balanced",
-        "performance" => " Performance",
-        _ => "",
-    };
+pub(crate) fn osd_power_profile(css_class: String, display_text: String) -> gtk::Label {
     gtk::Label::builder()
-        .css_classes(vec!["power", state.clone().as_str()])
-        .label(label)
+        .css_classes(vec!["power", css_class.clone().as_str()])
+        .label(display_text)
         .build()
 }
