3bc21d05f8f558186586ab7b4891f12a4da6ca65
A. Taha Baki <atahabaki.dev@pm.me>
2025-06-01 10:54:16 +0300

    add: customizable osd text for power_profile changes

--- src/model/config.rs
+++ src/model/config.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct Configuration {
-    duration: u64,
-    positioning: Positioning,
+    pub duration: u64,
+    pub positioning: Positioning,
+    pub osdtext: OsdText,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -37,6 +40,39 @@ impl From<Anchor> for gtkls::Edge {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct OsdText {
+    pub power_profile_text: PowerProfileText,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct PowerProfileText {
+    pub power_saver: String,
+    pub balanced: String,
+    pub performance: String,
+}
+
+impl PowerProfileText {
+    pub(crate) fn _get_based_on_new_profile_text(&self, new_profile: &String) -> String {
+        match new_profile.as_str() {
+            "power-saver" => self.power_saver.clone(),
+            "balanced" => self.balanced.clone(),
+            "performance" => self.performance.clone(),
+            _ => "".into(),
+        }
+    }
+}
+
+impl Default for PowerProfileText {
+    fn default() -> Self {
+        PowerProfileText {
+            power_saver: "  Power-Saver".to_owned(),
+            balanced: "  Balanced".to_owned(),
+            performance: " Performance".to_owned(),
+        }
+    }
+}
+
 impl Default for Configuration {
     fn default() -> Self {
         Self {
@@ -47,6 +83,9 @@ impl Default for Configuration {
                 bottom_margin: Some((Anchor::default(), 50)),
                 left_margin: None,
                 right_margin: None,
+            },
+            osdtext: OsdText {
+                power_profile_text: PowerProfileText::default(),
             },
         }
     }
