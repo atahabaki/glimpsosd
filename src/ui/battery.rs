f7091ceeb36cedd9f5ef97bf9da0c007c6f9028c
A. Taha Baki <atahabaki.dev@pm.me>
2025-06-02 13:34:17 +0300

    mv: ui::power_device -> ui::battery

--- src/ui/battery.rs
+++ src/ui/battery.rs
@@ -1,4 +1,4 @@
-pub(crate) fn osd_power_device(css_classes: Vec<String>, display_text: String) -> gtk::Label {
+pub(crate) fn osd_battery(css_classes: Vec<String>, display_text: String) -> gtk::Label {
     gtk::Label::builder()
         .css_classes(css_classes)
         .label(display_text)
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,4 @@
-pub(crate) mod power_device;
+pub(crate) mod battery;
 pub(crate) mod power_profiles;
-pub(crate) use power_device::osd_power_device;
+pub(crate) use battery::osd_battery;
 pub(crate) use power_profiles::osd_power_profile;
