c442ea9e94a9fd9799b197f77f37fd4aa7b42718
A. Taha Baki <atahabaki.dev@pm.me>
2025-06-02 04:30:08 +0300

    add&mv: +{get{,_max},set}_brightness & KbdBacklight -> KeyboardBacklight

--- src/model/keyboard_backlight.rs
+++ src/model/keyboard_backlight.rs
@@ -1,3 +1,4 @@
+use zbus::Result;
 use zbus::proxy;
 
 #[proxy(
@@ -5,7 +6,13 @@ use zbus::proxy;
     default_service = "org.freedesktop.UPower.KbdBacklight",
     default_path = "/org/freedesktop/UPower/KbdBacklight"
 )]
-pub(crate) trait KbdBacklight {
+pub(crate) trait KeyboardBacklight {
+    fn get_max_brightness(&self) -> Result<i32>;
+    fn get_brightness(&self) -> Result<i32>;
+    fn set_brightness(&self, brightness: i32) -> Result<()>;
+    /// This dbus signal only works if your keyboard backlight is
+    /// handled by this dbus service.
+    /// Otherwise it is handled mostly by the kernel.
     #[zbus(signal)]
     fn brightness_changed(&self) -> Result<i32>;
 }
