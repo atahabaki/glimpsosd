ec9295b66fe2e1acbca9b9261a865444dc601db2
A. Taha Baki <atahabaki.dev@pm.me>
2025-06-02 13:08:59 +0300

    add: percentage, is_present etc. props

--- src/model/power_device.rs
+++ src/model/power_device.rs
@@ -1,9 +1,19 @@
-#[zbus::proxy(
+use zbus::{Result, proxy};
+
+#[proxy(
     interface = "org.freedesktop.UPower.Device",
     default_service = "org.freedesktop.UPower",
-    default_path = "/org/freedesktop/UPower/Device"
+    default_path = "/org/freedesktop/UPower/devices/DisplayDevice"
 )]
 pub(crate) trait PowerDevice {
     #[zbus(property)]
-    fn state(&self) -> zbus::Result<u32>;
+    fn state(&self) -> Result<u32>;
+    #[zbus(property)]
+    fn percentage(&self) -> Result<f64>;
+    #[zbus(property)]
+    fn is_present(&self) -> Result<bool>;
+    #[zbus(property)]
+    fn type_(&self) -> Result<u32>;
+    #[zbus(property)]
+    fn power_supply(&self) -> Result<bool>;
 }
